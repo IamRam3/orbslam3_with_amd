04a0f7fac68896a24d513c94d7e568b6220202a8
IamRam3 <136904304+IamRam3@users.noreply.github.com>
2024-02-13 12:27:35 +0530

    Update build_container_cuda.sh

--- build_container_cuda.sh
+++ build_container_cuda.sh
@@ -20,9 +20,9 @@ touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 xhost +local:docker
-docker pull jahaniam/orbslam3:ubuntu20_noetic_cuda
+docker pull iamram4/armcuda_noetic:v1
 
-# docker pull jahaniam/orbslam3:ubuntu20_noetic_cuda
+# docker pull iamram4/armcuda_noetic:v1
 
 # Remove existing container
 docker rm -f orbslam3 &>/dev/null
@@ -41,7 +41,7 @@ docker run -td --privileged --net=host --ipc=host \
     -v `pwd`/Datasets:/Datasets \
     -v /etc/group:/etc/group:ro \
     -v `pwd`/ORB_SLAM3:/ORB_SLAM3 \
-    jahaniam/orbslam3:ubuntu20_noetic_cuda bash
+    iamram4/armcuda_noetic:v1 bash
 
 # Git pull orbslam and compile
 docker exec -it orbslam3 bash -i -c "git clone -b add_euroc_example.sh git@github.com:jahaniam/ORB_SLAM3.git /ORB_SLAM3 && cd /ORB_SLAM3 && chmod +x build.sh && ./build.sh "
