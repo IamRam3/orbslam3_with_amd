27a4f7f6722560d0855c05273ad9ad31271a0223
IamRam3 <136904304+IamRam3@users.noreply.github.com>
2024-02-16 16:52:35 +0530

    Update run_container_cuda .sh

--- run_container_cuda .sh
+++ run_container_cuda .sh
@@ -1,1 +1,13 @@
-
+docker run -td --privileged --net=host --ipc=host \
+    --name="orbslam3" \
+    --gpus=all \
+    -e "DISPLAY=$DISPLAY" \
+    -e "QT_X11_NO_MITSHM=1" \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -e "XAUTHORITY=$XAUTH" \
+    -e ROS_IP=127.0.0.1 \
+    --cap-add=SYS_PTRACE \
+    -v `pwd`/Datasets:/Datasets \
+    -v /etc/group:/etc/group:ro \
+    -v `pwd`/ORB_SLAM3:/ORB_SLAM3 \
+    sairam4/armcuda_noetic:v2 bash
