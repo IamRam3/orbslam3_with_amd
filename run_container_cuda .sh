f3c5f09a26ce3d4e96052cdd4ae427a3af8e3def
IamRam3 <136904304+IamRam3@users.noreply.github.com>
2024-02-16 16:54:43 +0530

    Update run_container_cuda .sh

--- run_container_cuda .sh
+++ run_container_cuda .sh
@@ -1,3 +1,34 @@
+# checking if you have nvidia
+if ! nvidia-smi | grep "Driver" 2>/dev/null; then
+  echo "******************************"
+  echo """It looks like you don't have nvidia drivers running. Consider running build_container_cpu.sh instead."""
+  echo "******************************"
+  while true; do
+    read -p "Do you still wish to continue?" yn
+    case $yn in
+      [Yy]* ) make install; break;;
+      [Nn]* ) exit;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+fi 
+
+# UI permisions
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+xhost +local:docker
+docker pull sairam4/armcuda_noetic:v2
+
+# docker pull sairam4/armcuda_noetic:v2
+
+# Remove existing container
+docker rm -f orbslam3 &>/dev/null
+[ -d "ORB_SLAM3" ] && sudo rm -rf ORB_SLAM3 && mkdir ORB_SLAM3
+
+# Create a new container
 docker run -td --privileged --net=host --ipc=host \
     --name="orbslam3" \
     --gpus=all \
@@ -11,3 +42,6 @@ docker run -td --privileged --net=host --ipc=host \
     -v /etc/group:/etc/group:ro \
     -v `pwd`/ORB_SLAM3:/ORB_SLAM3 \
     sairam4/armcuda_noetic:v2 bash
+
+# Install catkin build and noetic hector trajectory
+docker exec -it orbslam3 bash -i -c "sudo apt install ros-noetic-hector-trajectory-server && sudo apt install python3-catkin-tools"
