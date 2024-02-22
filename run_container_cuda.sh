90c48a4bc5e2cd3d5c9f2b0c626d494ae752d40a
IamRam3 <136904304+IamRam3@users.noreply.github.com>
2024-02-22 18:06:39 +0530

    Update run_container_cuda.sh

--- run_container_cuda.sh
+++ run_container_cuda.sh
@@ -20,9 +20,7 @@ touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 xhost +local:docker
-docker pull sairam4/armcuda_slam:v1
-
-# docker pull sairam4/armcuda_slam:v1
+# docker pull sairam4/patched_slam_nano_withoutframbuffer:cuda1
 
 # Remove existing container
 docker rm -f orbslam3 &>/dev/null
@@ -36,12 +34,13 @@ docker run -td --privileged --net=host --ipc=host \
     -e "QT_X11_NO_MITSHM=1" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -e "XAUTHORITY=$XAUTH" \
+    -e LD_LIBRARY_PATH="/usr/local/lib" \
     -e ROS_IP=127.0.0.1 \
     --cap-add=SYS_PTRACE \
     -v `pwd`/Datasets:/Datasets \
     -v /etc/group:/etc/group:ro \
     -v `pwd`/ORB_SLAM3:/ORB_SLAM3 \
     -v /run/user/1000/at-spi:/run/user/1000/at-spi \
-    sairam4/armcuda_slam:v1 bash
+    sairam4/patched_slam_nano_withoutframebuffer:cuda1 bash
 
 docker exec -it orbslam3 bash -i -c "cd ../ && sudo cp -r repo/slam_ws ORB_SLAM3"
